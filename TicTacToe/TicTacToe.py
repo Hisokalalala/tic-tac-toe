ca30365ebd3461721317350f0d57a62968532592
Hisokalalala <577833280@qq.com>
2020-12-22 21:13:18 +0900

    feat: implemented-turn

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,6 +1,13 @@
-def turn(array, m, n):
-    return ""
-
+def turn(array, who, m, n):
+    res = ""
+    if who == "M":
+        array[m-1][n-1] = "○"
+    else:
+        array[m-1][n-1] = "×"
+    for i in range(len(array)):
+        res += " ".join(array[i])
+        res += "\n"
+    return res
 
 def TicTacToe(m,n):
     return "Win"
