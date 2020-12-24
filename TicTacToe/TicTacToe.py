8291662610cfe0d8e75e3a9106f98b3a610abe76
Hisokalalala <577833280@qq.com>
2020-12-24 21:21:19 +0900

    implemented-CLI

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,3 +1,6 @@
+import argparse
+import sys
+
 def turn(array: list, who: str, m: int, n: int) -> str:
     res = ""
     if who == "M" and array[m-1][n-1] == "?":
@@ -12,14 +15,16 @@ def turn(array: list, who: str, m: int, n: int) -> str:
     return res
 
 
-def tictactoe() -> str:
+def tictactoe(now: str = "M") -> str:
     array = [["?", "?", "?"], ["?", "?", "?"], ["?", "?", "?"]]
     cnt = 0
-    now = "M"
     print("1 2のように座標を入力してください.")
     print("座標の分布としては以下のようになっております.")
     print("(1,1) (1,2) (1,3)\n(2,1) (2,2) (2,3)\n(3,1) (3,2) (3,3)\n")
-    print("先攻は○の人です")
+    if now == "M":
+        print("先攻は○の人です")
+    else:
+        print("先攻は×の人です")
     while cnt < 9:
         if (array[0] == ["○", "○", "○"] or array[1] == ["○", "○", "○"] or array[2] == ["○", "○", "○"] or
                 (array[0][0] == "○" and array[1][0] == "○" and array[2][0] == "○") or (
@@ -27,14 +32,14 @@ def tictactoe() -> str:
                 (array[0][2] == "○" and array[1][2] == "○" and array[2][2] == "○") or (
                         array[0][0] == "○" and array[1][1] == "○" and array[2][2] == "○") or
                 (array[0][2] == "○" and array[1][1] == "○" and array[2][0] == "○")):
-            return "result: Maru WIN!!"
+            return "result: ○ WIN!!"
         elif (array[0] == ["×", "×", "×"] or array[1] == ["×", "×", "×"] or array[2] == ["×", "×", "×"] or
               (array[0][0] == "×" and array[1][0] == "×" and array[2][0] == "×") or (
                       array[0][1] == "×" and array[1][1] == "×" and array[2][1] == "×") or
               (array[0][2] == "×" and array[1][2] == "×" and array[2][2] == "×") or (
                       array[0][0] == "×" and array[1][1] == "×" and array[2][2] == "×") or
               (array[0][2] == "×" and array[1][1] == "×" and array[2][0] == "×")):
-            return "result: Batsu WIN!!"
+            return "result: × WIN!!"
         else:
             if now == "M":
                 print("今は○の人のターンです")
@@ -42,7 +47,7 @@ def tictactoe() -> str:
                 print("今は×の人のターンです")
             try:
                 x, y = (int(m) for m in input().split())
-            except ValueError as e:
+            except (ValueError, IndexError) as e:
                 print("座標の入力形式が間違っています、入力し直してください")
                 x, y = (int(m) for m in input().split())
             if now == "M":
@@ -52,7 +57,7 @@ def tictactoe() -> str:
                     print(e)
                     try:
                         x, y = (int(m) for m in input().split())
-                    except ValueError as e:
+                    except (ValueError, IndexError) as e:
                         print("座標の入力形式が間違っています、入力し直してください")
                         x, y = (int(m) for m in input().split())
                     situation = turn(array, "M", x, y)
@@ -66,7 +71,7 @@ def tictactoe() -> str:
                     print(e)
                     try:
                         x, y = (int(m) for m in input().split())
-                    except ValueError as e:
+                    except (ValueError, IndexError) as e:
                         print("座標の入力形式が間違っています、入力し直してください")
                         x, y = (int(m) for m in input().split())
                     situation = turn(array, "B", x, y)
@@ -76,6 +81,11 @@ def tictactoe() -> str:
     else:
         return "result: DRAW"
 
-a = tictactoe()
-print(a)
-print("GameEnd")
+def main():
+    parser = argparse.ArgumentParser(description='TicTacToe program.')
+    parser.add_argument('--now', type=str, default="M",
+                        help='先攻を決めます。○が先なら"M"を×が先なら"B"を引数として入れてください')
+    args = parser.parse_args()
+
+    print(tictactoe(now=args.now))
+    print("GameEND")
